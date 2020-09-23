7a76a90eeb69cded62d54561c28cb3fee3eee8f8
FransTan <fransiskus847@gmail.com>
2020-09-23 17:57:36 +0800

    Upload lewat git-scm

---
+++ Biodata Diri.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("=== BIODATA DIRI ===\n");
+	printf("Nama 	: Fransiskus Jonathan Tanudjaja\n");
+	printf("NPM 	: 19081010062\n");
+	printf("Alamat	: Jalan Gatot Subroto Gg. Merpati No.6A Rt.34\n");
+	printf("Tempat, tanggal lahir : Banjarmasin, 17 April 2001\n");
+	printf("\nLatar Belakang Pendidikan : \n1. 2007-2013 : SD Hippindo \n2. 2013-2016 : SMP Negeri 6 Banjarmasin \n3. 2016-2019 : SMA Negeri 7 Banjarmasin \n4. 2019-Sekarang : Pendidikan Sarjana UPNV Jatim");
+
+	return 0;
+}
